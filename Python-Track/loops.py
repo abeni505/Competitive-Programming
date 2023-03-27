9582d3be8cd3f3f81076e6f44de7ddd37896a93c
Abenezer Mulugeta <70083175+abeni505@users.noreply.github.com>
2023-03-27 21:41:18 +0300

    Solved https://www.hackerrank.com/challenges/python-loops/problem?isFullScreen=true

--- Python-Track/loops.py
+++ Python-Track/loops.py
@@ -1,10 +1,6 @@
 if __name__ == '__main__':
     n = int(input())
-arr=[]
-i=0
-while i < n:
-    arr.append(i)
-    i+=1
-for i in arr:
-    print((arr[i])**2)
+    
+for i in range(n):
+    print(i**2)
     
