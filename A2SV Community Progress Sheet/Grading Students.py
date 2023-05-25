a978e226a539002a43dfb5a49bde0f70fd5ec25e
Abenezer Mulugeta <70083175+abeni505@users.noreply.github.com>
2023-05-25 22:52:45 +0300

    Solved https://www.hackerrank.com/challenges/grading/problem?isFullScreen=true

---
+++ A2SV Community Progress Sheet/Grading Students.py
@@ -0,0 +1,47 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'gradingStudents' function below.
+#
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts INTEGER_ARRAY grades as parameter.
+#
+
+def gradingStudents(grades):
+    # Write your code here
+    arr = []
+    for i in grades:
+        if i>=38:
+            if i%5==4:
+                i+=1
+            elif i%5==3:
+                i+=2
+            
+        arr.append(i)
+    return arr
+            
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    grades_count = int(input().strip())
+
+    grades = []
+
+    for _ in range(grades_count):
+        grades_item = int(input().strip())
+        grades.append(grades_item)
+
+    result = gradingStudents(grades)
+
+    fptr.write('\n'.join(map(str, result)))
+    fptr.write('\n')
+
+    fptr.close()
