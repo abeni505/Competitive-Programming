82b8dbd3c094141d8568f981f0b7fcaaf67aa2e5
Abenezer Mulugeta <70083175+abeni505@users.noreply.github.com>
2023-06-02 00:15:50 +0300

    Solved https://www.hackerrank.com/challenges/insertionsort2/problem?isFullScreen=true

---
+++ Insertion Sort - Part 2.py
@@ -0,0 +1,33 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'insertionSort2' function below.
+#
+# The function accepts following parameters:
+#  1. INTEGER n
+#  2. INTEGER_ARRAY arr
+#
+
+def insertionSort2(n, arr):
+    # Write your code here
+    for i in range(1,n):
+        key=arr[i]
+        
+        while arr[i-1]>key and i>0:
+            arr[i]=arr[i-1]
+            i -= 1
+        arr[i]=key
+        print(*arr)
+
+if __name__ == '__main__':
+    n = int(input().strip())
+
+    arr = list(map(int, input().rstrip().split()))
+
+    insertionSort2(n, arr)
