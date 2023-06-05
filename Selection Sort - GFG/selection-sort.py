1c7cea68fde6b44c1380d7584558265a4e9f7383
Abenezer Mulugeta <70083175+abeni505@users.noreply.github.com>
2023-06-05 19:47:02 +0300

    Added solution - LeetHub

--- Selection Sort - GFG/selection-sort.py
+++ Selection Sort - GFG/selection-sort.py
@@ -4,20 +4,21 @@ class Solution:
     def select(self, arr, i):
         # code here 
         min_index=i
-        for j in range(i+1,len(arr)):
-            if arr[min_index]>arr[j]:
-                min_index=j
-                
+        for x in range(i+1,len(arr)):
+            if arr[x]<arr[min_index]:
+                min_index=x
+        
         return min_index
             
     
     def selectionSort(self, arr,n):
         #code here
-        for i in range(n):
-            min_index=self.select(arr,i)
-            arr[i],arr[min_index]=arr[min_index],arr[i]
-            
+        for z in range(n):
+            minimum=self.select(arr,z)
+            arr[z],arr[minimum]=arr[minimum],arr[z]
+        
         return arr
+
 
 #{ 
  # Driver Code Starts
