af0696edad9b5b8f9dd8220b3f084a1e6659e142
Abenezer Mulugeta <70083175+abeni505@users.noreply.github.com>
2023-06-09 10:14:45 +0300

    Time: 7 ms (99.63%), Space: 13.3 MB (77.35%) - LeetHub

--- 1894-merge-strings-alternately/1894-merge-strings-alternately.py
+++ 1894-merge-strings-alternately/1894-merge-strings-alternately.py
@@ -7,13 +7,15 @@ class Solution(object):
         """
         ptr1=ptr2=0
         merge=""
-        while ptr1<len(word1) and ptr2<len(word2):
-            merge+=word1[ptr1]
-            merge+=word2[ptr2]
-            ptr1+=1
-            ptr2+=1
-        merge+=str(word1[ptr1:])
-        merge+=str(word2[ptr2:])
+        while ptr1<len(word1) or ptr2<len(word2):
+            if (ptr1<len(word1) and ptr2==len(word2))or(ptr1==len(word1) and ptr2<len(word2)):
+                merge+=word1[ptr1:]
+                merge+=word2[ptr2:]
+                break
+            else:
+                merge+=word1[ptr1]
+                merge+=word2[ptr2]
+                ptr1+=1
+                ptr2+=1
         
-
         return merge
