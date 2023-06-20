739e673f522996830837423d8ed736306c056a16
Abenezer Mulugeta <70083175+abeni505@users.noreply.github.com>
2023-06-20 22:18:52 +0300

    Time: 105 ms (5.11%), Space: 17 MB (63.1%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -1,8 +1,21 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        newstr=""
-        for i in s:
-            if i.isalnum():
-                newstr+=i.lower()
-        return newstr==newstr[::-1]
-        
+
+        left,right = 0,len(s)-1
+
+        while left<right:
+            while left<right and not self.isalphanum(s[left]):
+                left+=1
+            while right>left and not self.isalphanum(s[right]):
+                right-=1
+            if s[left].lower() != s[right].lower():
+                return False
+            left+=1
+            right-=1
+        return True
+
+
+    def isalphanum(self,c):
+        return (ord("0")<=ord(c)<=ord("9") or
+        ord("A")<=ord(c)<=ord("Z") or
+        ord("a")<=ord(c)<=ord("z"))
