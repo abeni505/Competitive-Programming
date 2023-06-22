51907cacc02940829910d79d505cb0f4abfc36ea
Abenezer Mulugeta <70083175+abeni505@users.noreply.github.com>
2023-06-22 09:43:31 +0300

    Time: 627 ms (8.57%), Space: 16.8 MB (64.33%) - LeetHub

--- 2502-sort-the-people/2502-sort-the-people.py
+++ 2502-sort-the-people/2502-sort-the-people.py
@@ -1,14 +1,22 @@
 class Solution:
     def sortPeople(self, names: List[str], heights: List[int]) -> List[str]:
-        
+
+
         for i in range(len(heights)):
-            swapped=False
-            for j in range(len(heights)-1-i):
-                if heights[j]<heights[j+1]:
-                    heights[j],heights[j+1]=heights[j+1],heights[j]
-                    names[j],names[j+1]=names[j+1],names[j]
-                    swapped=True
-            if not swapped:
-                break
-           
+            max_index=i          
+            for j in range(i+1,len(heights)):
+                if heights[j]>heights[max_index]:
+                    max_index=j
+            heights[i],heights[max_index]=heights[max_index],heights[i]
+            names[i],names[max_index]=names[max_index],names[i]
+
         return names
+        
+
+        
+
+        
+       
+
+
+            
