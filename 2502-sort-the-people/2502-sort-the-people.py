dcf750e36858420b1187117b78fcf398c276646b
Abenezer Mulugeta <70083175+abeni505@users.noreply.github.com>
2023-10-14 10:49:51 +0300

    Time: 690 ms (6.4%), Space: 16.7 MB (95.11%) - LeetHub

--- 2502-sort-the-people/2502-sort-the-people.py
+++ 2502-sort-the-people/2502-sort-the-people.py
@@ -1,23 +1,24 @@
 class Solution:
     def sortPeople(self, names: List[str], heights: List[int]) -> List[str]:
-        
 
-        hashmap={}
+        for i in range(len(heights)):
 
-        for x in range(len(heights)):
-            hashmap[heights[x]]=names[x]
+            key=heights[i]
+            key_name=names[i]
+            j=i-1
+            
+            while j>=0 and heights[j]<key:
+                heights[j+1]=heights[j]
+                names[j+1]=names[j]
 
-        h=max(heights)+1
-        counter=[0]*h
-        sorted_arr=[]
-        for i in heights:
-            counter[i]+=1
+                heights[j]=key
+                names[j]=key_name
+                j-=1
 
-        for j in range(len(counter)-1,-1,-1):
-            for k in range(counter[j]):
-                sorted_arr.append(hashmap[j])
+        return names
 
-        return sorted_arr
+
+
 
 
 
