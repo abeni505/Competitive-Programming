9b46480c96982defc288e59f8116e134c92a29db
Abenezer Mulugeta <70083175+abeni505@users.noreply.github.com>
2023-11-29 15:14:50 +0300

    Added solution for Toeplitz Matrix

--- Python-Track/toeplitz-matrix.py
+++ Python-Track/toeplitz-matrix.py
@@ -1,9 +1,10 @@
 class Solution:
     def isToeplitzMatrix(self, matrix: List[List[int]]) -> bool:
-        for row in range(len(matrix)-1):
-            for column in range(len(matrix[row])-1):
+        for row in range(1,len(matrix)):
+            for column in range(1,len(matrix[row])):
                 
-                if matrix[row][column] != matrix[row+1][column+1]:
+                if matrix[row][column] != matrix[row-1][column-1]:
                     return False
+            
         return True
                     
