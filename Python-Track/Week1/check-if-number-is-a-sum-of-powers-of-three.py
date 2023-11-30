f043a9a9e65ce27e234a722a92130f6c316647a4
Abenezer Mulugeta <70083175+abeni505@users.noreply.github.com>
2023-11-30 11:28:34 +0300

    Added solution for Check if Number is a Sum of Powers of Three

---
+++ Python-Track/Week1/check-if-number-is-a-sum-of-powers-of-three.py
@@ -0,0 +1,10 @@
+class Solution:
+    def checkPowersOfThree(self, n: int) -> bool:
+        
+        while n > 1 :
+            
+            if n % 3 == 2:
+                return False
+    
+            n //= 3
+        return True
