539df564ed61d564ff16e0eac6c780cf7e6a2d19
Abenezer Mulugeta <70083175+abeni505@users.noreply.github.com>
2023-12-01 19:48:33 +0300

    Time: 46 ms (83.67%) | Memory: 16.3 MB (24.22%) - LeetSync

--- 13-roman-to-integer/roman-to-integer.py
+++ 13-roman-to-integer/roman-to-integer.py
@@ -1,50 +1,25 @@
 class Solution:
     def romanToInt(self, s: str) -> int:
-        dict_ = {"I":1,"V":5,"X":10,"L":50,"C":100,"D":500,"M":1000}
+        
+        dict1 = {"I":1,"V":5,"X":10,"L":50,"C":100,"D":500,"M":1000}
+        dict2 = {"I":1,"V":2,"X":3,"L":4,"C":5,"D":6,"M":7}
 
+        window = {"I","X","C"}
         total_sum = 0
         i = 0
-        while i < len(s)-1:
-            if s[i] == "I" and s[i+1] == "V":
-                total_sum += 4
-                i+=1
 
-            elif s[i] == "I" and s[i+1] == "X":
-                total_sum += 9
-                i+=1
+        while i < len(s):
+            if s[i] not in window:
+                total_sum += dict1[s[i]]
 
-            elif s[i] == "X" and s[i+1] == "L":
-                total_sum += 40
-                i+=1
-
-            elif s[i] == "X" and s[i+1] == "C":
-                total_sum += 90
-                i+=1
-
-            elif s[i] == "C" and s[i+1] == "D":
-                total_sum += 400
-                i+=1
-            elif s[i] == "C" and s[i+1] == "M":
-                total_sum += 900
-                i+=1
-            elif s[i] == "I" and s[i+1] == "I":
-                total_sum +=  len(s[i:])
-                i = len(s)
-                break
-                
             else:
-                # if not s[i+1]:
-                #     total_sum += dict_[s[i]]
-                #     total_sum += dict_[s[i+1]]
-                # else:
-                #     total_sum += s[i]
-                total_sum += dict_[s[i]]
-                    
+                if i+1 < len(s) and dict2[s[i]] < dict2[s[i+1]]:
+                    total_sum += dict1[s[i+1]]- dict1[s[i]]
+                    i += 1
+                else:
+                    total_sum += dict1[s[i]]
             i += 1
-            print(total_sum)
-        if i < len(s):
-            total_sum += dict_[s[i]]
-        
-        
+
         return total_sum
-          
+
+
