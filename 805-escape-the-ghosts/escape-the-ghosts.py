f2c90a17b286ce3a1d330f55f6a0672e878eefa3
Abenezer Mulugeta <70083175+abeni505@users.noreply.github.com>
2023-12-05 14:52:09 +0300

    Time: 43 ms (98.02%) | Memory: 16.4 MB (24.21%) - LeetSync

--- 805-escape-the-ghosts/escape-the-ghosts.py
+++ 805-escape-the-ghosts/escape-the-ghosts.py
@@ -6,7 +6,7 @@ class Solution:
         for i in range(len(ghosts)):
             x1, y1 = ghosts[i]
             x2, y2 = target[0],target[1]
-            print(x2,y2)
+            
             if abs(x2 - x1) + abs(y2 - y1) <= my:
                 return False
 
