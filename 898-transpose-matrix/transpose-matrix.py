5262d196e32a9a7e59cc76f854d14ebb618cc330
Abenezer Mulugeta <70083175+abeni505@users.noreply.github.com>
2023-12-10 13:16:48 +0300

    Time: 75 ms (49.73%) | Memory: 17.4 MB (7.84%) - LeetSync

--- 898-transpose-matrix/transpose-matrix.py
+++ 898-transpose-matrix/transpose-matrix.py
@@ -3,13 +3,5 @@ class Solution:
 
         row = len(matrix)
         col = len(matrix[0])
-        out_put = []
-       
-        for i in range(col):
-            new_row = []
-            for j in range(row):
-                new_row.append( matrix[j][i] )
 
-            out_put.append(new_row)
-            
-        return out_put
+        return [[matrix[j][i] for j in range(row)] for i in range(col)]
