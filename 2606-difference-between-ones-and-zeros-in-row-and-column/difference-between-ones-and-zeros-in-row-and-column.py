9fe61f4e8606bf8fd206a2b4d8829c009d049168
Abenezer Mulugeta <70083175+abeni505@users.noreply.github.com>
2023-12-15 02:22:11 +0300

    Time: 1389 ms (61.99%) | Memory: 51.3 MB (69.88%) - LeetSync

--- 2606-difference-between-ones-and-zeros-in-row-and-column/difference-between-ones-and-zeros-in-row-and-column.py
+++ 2606-difference-between-ones-and-zeros-in-row-and-column/difference-between-ones-and-zeros-in-row-and-column.py
@@ -6,15 +6,22 @@ class Solution:
 
         row_1 = [0] * m
         col_1 = [0] * n
+        row_0 = [0] * m
+        col_0 = [0] * n
+        
+        out_put = [[0] * n for _ in range(m)]
+        for i in range(m):
+            for j in range(n):
+                if grid[i][j]:
+                    row_1[i] += 1
+                    col_1[j] += 1
+                else:
+                    row_0[i] += 1
+                    col_0[j] += 1
 
         for i in range(m):
             for j in range(n):
-                row_1[i] += grid[i][j]
-                col_1[j] += grid[i][j]
+                out_put[i][j] = row_1[i] + col_1[j] - row_0[i] - col_0[j]
 
-        for i in range(m):
-            for j in range(n):
-                grid[i][j] = 2 * (row_1[i] + col_1[j]) - m - n
-
-        return grid
+        return out_put
 
