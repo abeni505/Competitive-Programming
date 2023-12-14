b4572f88c2d6114f49286053100bb2fb4e7c56f8
Abenezer Mulugeta <70083175+abeni505@users.noreply.github.com>
2023-12-15 01:12:30 +0300

    Time: 107 ms (22.62%) | Memory: 16.5 MB (84.26%) - LeetSync

--- 842-card-flipping-game/card-flipping-game.py
+++ 842-card-flipping-game/card-flipping-game.py
@@ -2,13 +2,6 @@ class Solution:
     def flipgame(self, fronts: List[int], backs: List[int]) -> int:
         
       
-        hashset = set(fronts+backs)
-        
-        for i in range(len(fronts)):
-            if fronts[i] == backs[i]:
-                hashset.discard(fronts[i])
-
-        if hashset:
-            return(min(hashset))
-            
-        return 0
+        hashset = set(fronts+backs) - set(a for a, b in zip(fronts,backs) if a == b)
+    
+        return min(hashset) if hashset else 0
