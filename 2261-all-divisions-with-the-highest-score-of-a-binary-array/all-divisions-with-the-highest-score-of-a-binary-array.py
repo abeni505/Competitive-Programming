4016ded53b7f3303c8d3bbc6fc7d5e810d3627ed
Abenezer Mulugeta <70083175+abeni505@users.noreply.github.com>
2023-12-21 19:10:41 +0300

    Time: 2536 ms (93.86%) | Memory: 42.2 MB (20.24%) - LeetSync

---
+++ 2261-all-divisions-with-the-highest-score-of-a-binary-array/all-divisions-with-the-highest-score-of-a-binary-array.py
@@ -0,0 +1,33 @@
+class Solution:
+    def maxScoreIndices(self, nums: List[int]) -> List[int]:
+        
+        m_score = dict()
+        score = sum(nums)
+        m_score[0] = score
+        
+        total_sum = 0
+        for i in range(0,len(nums)-1):
+            cur_score = 0
+            total_sum += nums[i]
+            nums_left = (i+1) - total_sum
+            nums_right = score - total_sum
+
+            cur_score = nums_left + nums_right
+            m_score[i+1] = cur_score
+
+        m_score[len(nums)] = len(nums) - sum(nums)
+
+        max_val = 0
+        for key, val in m_score.items():
+            if val > max_val:
+                max_val = val
+
+        output = []
+        for key, val in m_score.items():
+            if val == max_val:
+                output.append(key)
+        
+        return output
+            # score = nums_left + nums_right
+            # max_score = max(max_score,score)
+            # m_score[score] = i
