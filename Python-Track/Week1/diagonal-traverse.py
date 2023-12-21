bde13a4d6a6e8eb13eb68d3741ea81942eb6385a
Abenezer Mulugeta <70083175+abeni505@users.noreply.github.com>
2023-12-22 00:04:42 +0300

    Added solution for Diagonal Traverse

--- Python-Track/Week1/diagonal-traverse.py
+++ Python-Track/Week1/diagonal-traverse.py
@@ -1,22 +1,22 @@
 class Solution:
     def findDiagonalOrder(self, mat: List[List[int]]) -> List[int]:
         
-        x = defaultdict(list)
+        diagonals = defaultdict(list)
 
         m = len(mat)
         n = len(mat[0])
+
         for i in range(m):
             for j in range(n):
-                x[i+j].append(mat[i][j])
+                diagonals[i+j].append(mat[i][j])
 
-        output = [*x[0]]
-        for key, val in x.items():
-            if key > 0 and key % 2 != 0:
-                for i in x[key]:
-                    output.append(i)
-            elif key > 0 and key % 2 == 0:
-                reverse = x[key]
-                for i in reverse[::-1]:
-                    output.append(i)
+        output = []
 
+        for key, val in diagonals.items():
+            if  key % 2 != 0:
+                output.extend(diagonals[key])
+                
+            else:   
+                output.extend(diagonals[key][::-1])
+                
         return output
