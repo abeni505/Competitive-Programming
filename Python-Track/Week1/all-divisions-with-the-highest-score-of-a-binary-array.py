34e53de0f244a505740061750bc4f1f57de888b8
Abenezer Mulugeta <70083175+abeni505@users.noreply.github.com>
2023-12-21 19:44:44 +0300

    Added solution for All Divisions With the Highest Score of a Binary Array

--- Python-Track/Week1/all-divisions-with-the-highest-score-of-a-binary-array.py
+++ Python-Track/Week1/all-divisions-with-the-highest-score-of-a-binary-array.py
@@ -1,32 +1,28 @@
 class Solution:
     def maxScoreIndices(self, nums: List[int]) -> List[int]:
         
-        m_score = { 0 : sum(nums), len(nums):len(nums) - sum(nums) }
         score = sum(nums)
-       
+
+        m_score = defaultdict(list) 
+        m_score[score].append(0)
+        m_score[ len(nums) - score].append(len(nums))
+
         
         total_sum = 0
-        for i in range(0,len(nums)-1):
+        for i in range(len(nums)-1):
             cur_score = 0
             total_sum += nums[i]
             nums_left = (i+1) - total_sum
             nums_right = score - total_sum
 
             cur_score = nums_left + nums_right
-            m_score[i+1] = cur_score
+            m_score[cur_score].append(i+1) 
 
 
-        max_val = 0
-        for key, val in m_score.items():
-            if val > max_val:
-                max_val = val
+        max_val = max(m_score.keys())
 
-        output = []
-        for key, val in m_score.items():
-            if val == max_val:
-                output.append(key)
-        
-        return output
+    
+        return m_score[max_val]
             # score = nums_left + nums_right
             # max_score = max(max_score,score)
             # m_score[score] = i
