d770138bc494cb450071fdda8ce337b1dbdf8cb9
Abenezer Mulugeta <70083175+abeni505@users.noreply.github.com>
2023-12-24 10:30:46 +0300

    Time: 33 ms (96.17%) | Memory: 17.2 MB (5.30%) - LeetSync

--- 48-rotate-image/rotate-image.py
+++ 48-rotate-image/rotate-image.py
@@ -4,17 +4,16 @@ class Solution:
         Do not return anything, modify matrix in-place instead.
         """
         
-       
+
         n = len(matrix)
-    
-        for c in range(n):
-            for r in range(n // 2): # to not swap again 
-                matrix[r][c], matrix[n - 1 - r][c] = matrix[n - 1 - r][c], matrix[r][c]
 
-        for row in range(n):
-            for col in range(row + 1): # to swap from above main digonal
-                matrix[row][col] , matrix[col][row] = matrix[col][row] , matrix[row][col]
+        for r in range(n):
+            for c in range(r + 1):
+                matrix[r][c] , matrix[c][r] = matrix[c][r] , matrix[r][c]
 
-        # for row in matrix:
-        #     print(row)
+        for row in matrix:
+            row.reverse()
+
+        
+        
         
