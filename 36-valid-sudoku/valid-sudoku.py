b8f883f5ff73c4587144f50cb266f3947c9475ef
Abenezer Mulugeta <70083175+abeni505@users.noreply.github.com>
2023-12-26 00:22:34 +0300

    Time: 108 ms (23.67%) | Memory: 17.4 MB (5.15%) - LeetSync

--- 36-valid-sudoku/valid-sudoku.py
+++ 36-valid-sudoku/valid-sudoku.py
@@ -1,8 +1,5 @@
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
-        
-
-        
      
         for i in board:
             count_row = Counter(i)
@@ -20,14 +17,18 @@ class Solution:
                 if key != "." and val > 1:
                     return False
 
-        sub_box = defaultdict(set)
-
         for row in range(9):
             for col in range(9):
-                if board[row][col] != "." and  board[row][col] in sub_box[(row//3,col//3)]:
-                    return False
                 
-                sub_box[(row//3 , col//3)].add(board[row][col])
-
+                count = 0
+                row_index = row // 3
+                col_index = col // 3
+                
+                for r in range(row_index * 3 , (row_index * 3) + 3):
+                    for c in range(col_index * 3 ,(col_index * 3) + 3):
+                        if board[r][c] != "." and board[r][c] == board[row][col]:
+                            if count >= 1:
+                                return False
+                            count += 1
 
         return True
