063467d3c6d8f23ba29b8233fe1b76206bc63147
Abenezer Mulugeta <70083175+abeni505@users.noreply.github.com>
2024-01-02 15:08:32 +0300

    Time: 740 ms (27.72%) | Memory: 23.7 MB (20.94%) - LeetSync

--- 1014-k-closest-points-to-origin/k-closest-points-to-origin.py
+++ 1014-k-closest-points-to-origin/k-closest-points-to-origin.py
@@ -3,8 +3,4 @@ class Solution:
         
         points.sort(key = lambda x : sqrt(x[0]**2 + x[1]**2))
 
-        output = []
-        for i in range(k):
-            output.append(points[i])
-
-        return output
+        return points[:k]
