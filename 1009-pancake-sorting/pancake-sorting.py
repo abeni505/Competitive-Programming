1e22db0ff5a4ac5316936e8b7b9e1582f1694d1e
Abenezer Mulugeta <70083175+abeni505@users.noreply.github.com>
2024-01-02 15:38:10 +0300

    Time: 85 ms (7.10%) | Memory: 17.3 MB (30.13%) - LeetSync

--- 1009-pancake-sorting/pancake-sorting.py
+++ 1009-pancake-sorting/pancake-sorting.py
@@ -1,24 +1,25 @@
 class Solution:
     def pancakeSort(self, arr: List[int]) -> List[int]:
-
-        def flip(array , k):
+        
+        def flip(array , index):
             left = 0
-            right = k
+            right = index 
 
             while left < right:
-                array[left] , array[right] = array[right] , array[left]
+                array[left] , array[right] = array[right], array[left]
                 left += 1
                 right -= 1
         
+
         output = []
-        for i in range(len(arr) - 1 , -1 , -1):
+        for i in range(len(arr)-1,-1,-1):
+
             max_index = arr.index(i + 1)
 
-            flip(arr,max_index)
+            flip(arr, max_index)
             output.append(max_index + 1)
 
             flip(arr, i)
             output.append(i + 1)
 
         return output
-
