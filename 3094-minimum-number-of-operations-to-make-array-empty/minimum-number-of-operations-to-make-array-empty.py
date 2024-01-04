18c2e29cb21a2c84e3751031a13a70fc14d5f806
Abenezer Mulugeta <70083175+abeni505@users.noreply.github.com>
2024-01-04 15:44:11 +0300

    Time: 570 ms (90.30%) | Memory: 31.4 MB (23.03%) - LeetSync

---
+++ 3094-minimum-number-of-operations-to-make-array-empty/minimum-number-of-operations-to-make-array-empty.py
@@ -0,0 +1,25 @@
+class Solution:
+    def minOperations(self, nums: List[int]) -> int:
+
+        nums.sort()
+        count = Counter(nums)
+
+        total = 0
+        for val in count.values():
+            if val >= 3:
+                q = val//3
+                r = val % 3
+                if r > 0:
+                    r = 1
+                total += q + r
+        
+            elif val == 2:
+                q = val//2
+                r = val % 2
+                total += q + r
+            else:
+                total = -1
+                break
+
+
+        return total
