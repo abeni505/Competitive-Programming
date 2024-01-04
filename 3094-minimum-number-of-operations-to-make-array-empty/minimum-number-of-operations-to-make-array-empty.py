05f4188ef15f012a531180964a260c8d9f819ac9
Abenezer Mulugeta <70083175+abeni505@users.noreply.github.com>
2024-01-04 15:56:08 +0300

    Time: 539 ms (97.27%) | Memory: 31.8 MB (12.12%) - LeetSync

--- 3094-minimum-number-of-operations-to-make-array-empty/minimum-number-of-operations-to-make-array-empty.py
+++ 3094-minimum-number-of-operations-to-make-array-empty/minimum-number-of-operations-to-make-array-empty.py
@@ -5,18 +5,14 @@ class Solution:
 
         total = 0
         for val in count.values():
-            if val >= 2:
+            if val == 1:
+                return -1
+           
+            q = val//3
+            r = val % 3
+            if r > 0 or q == 0:
+                r = 1
 
-                q = val//3
-                r = val % 3
-                if r > 0 or q == 0:
-                    r = 1
-
-                total += q + r
-
-            else:
-                total = -1
-                break
-
+            total += q + r
 
         return total
