3ab650f85c158cd28f8cedb5037a4a35d00f9d41
Abenezer Mulugeta <70083175+abeni505@users.noreply.github.com>
2024-01-05 19:47:55 +0300

    Time: 30 ms (95.47%) | Memory: 17.3 MB (7.23%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.py
+++ 1013-fibonacci-number/fibonacci-number.py
@@ -1,10 +1,16 @@
-def fibonacci(n):
+def fibonacci(n, hashmap):
         if n <= 1:
             return n  
-        return  fibonacci(n - 1) + fibonacci(n - 2)
-        
+
+        if n in hashmap:
+            return hashmap[n]
+
+        hashmap[n] = fibonacci(n - 1 , hashmap) + fibonacci(n - 2, hashmap)
+
+        return  hashmap[n]
+
 class Solution:
     def fib(self, n: int) -> int:
-        return  fibonacci(n)
+        return  fibonacci(n,{})
 
     
