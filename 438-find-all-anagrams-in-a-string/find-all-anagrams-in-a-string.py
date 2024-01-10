c3b3c899bc6acd827378a89d7d3d0357045c77b3
Abenezer Mulugeta <70083175+abeni505@users.noreply.github.com>
2024-01-10 19:20:50 +0300

    Time: 225 ms (37.67%) | Memory: 18.6 MB (17.18%) - LeetSync

--- 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.py
+++ 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.py
@@ -2,21 +2,25 @@ class Solution:
     def findAnagrams(self, s: str, p: str) -> List[int]:
         
         k = len(p)
-        countp = Counter(p)
-
-        check = deque()
         output = []
-        left = right = 0
-        while right < len(s):
-            if right - left <= k:
-                check.append(s[right])
-                right += 1
-                # print(check)
-                if  right - left == k:
-                    counts = Counter(check)
-                    check.popleft()
-                    left += 1
-                    if counts == countp:
-                        output.append(right - k)
+    
+        
+        count_target = Counter(p)
+        count_check = Counter(s[:k])
+
+        if count_check == count_target:
+                output.append(0)
+        
+        for i in range(k ,len(s)):
+            cur_char = s[i]
+            count_check[cur_char] += 1
+            count_check[s[i - k]] -= 1
+
+            if count_check == count_target:
+                output.append(i - k + 1)
+
         
         return output
+
+
+
