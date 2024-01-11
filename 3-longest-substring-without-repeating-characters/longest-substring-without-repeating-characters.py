0bf8c15aafd8c99e7e947d2a4044c0a73846234b
Abenezer Mulugeta <70083175+abeni505@users.noreply.github.com>
2024-01-11 13:55:38 +0300

    Time: 58 ms (69.78%) | Memory: 17.5 MB (6.29%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
@@ -7,7 +7,7 @@ class Solution:
 
         for right in range(len(s)):
             while s[right] in window:
-                max_len = max(max_len, right - left)
+               
                 window.remove(s[left])
                 left += 1
             window.add(s[right])
