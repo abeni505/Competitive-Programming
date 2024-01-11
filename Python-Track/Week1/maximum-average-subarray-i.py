4e4d0df50f8e6db46d05c70e80b48ddd1535e27f
Abenezer Mulugeta <70083175+abeni505@users.noreply.github.com>
2024-01-11 16:08:41 +0300

    Added solution for Maximum Average Subarray I

--- Python-Track/Week1/maximum-average-subarray-i.py
+++ Python-Track/Week1/maximum-average-subarray-i.py
@@ -1,20 +1,16 @@
 class Solution:
     def findMaxAverage(self, nums: List[int], k: int) -> float:
         
-        left = right = 0
+        window_sum = sum(nums[:k])
      
-        max_avg = float('-inf')
-        cur_sum = 0
-        
-        while right < len(nums):
-            if right - left  <= k:
-                cur_sum += nums[right]
-                right += 1
-                
-                if right - left == k:
-                    max_avg = max(max_avg,cur_sum/k)
-                    cur_sum -= nums[left]
-                    left += 1
-   
+        max_avg = window_sum/k
+      
+        for i in range(k , len(nums)):
+            window_sum += nums[i]
+            window_sum -= nums[i-k]
+            max_avg = max(max_avg , window_sum/k)
+
         return max_avg
 
+        
+        
