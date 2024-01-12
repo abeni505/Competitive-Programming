501397c98dfc3f54e1e29177a8f21519ad638da6
Abenezer Mulugeta <70083175+abeni505@users.noreply.github.com>
2024-01-12 19:43:21 +0300

    Added solution for Permutation in String

---
+++ Python-Track/Week1/permutation-in-string.py
@@ -0,0 +1,25 @@
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        k = len(s1)
+        left = 0
+        window = deque()
+        window.extend(s2[:k])
+        count = Counter(s1)
+
+        for i in range(k):
+            if Counter(window) == count:
+                return True
+        
+        for right in range(k , len(s2)):
+            window.popleft()
+            left += 1
+            window.append(s2[right])
+            if Counter(window) == count:
+                return True
+        
+        return False
+
+
+
+
+
