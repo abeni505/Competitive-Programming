57b70a88bb100585ef5d4343ea836992df6fc4f7
Abenezer Mulugeta <70083175+abeni505@users.noreply.github.com>
2024-01-12 21:26:20 +0300

    Time: 58 ms (85.59%) | Memory: 17.5 MB (21.78%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.py
+++ 567-permutation-in-string/permutation-in-string.py
@@ -2,22 +2,25 @@ class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
         k = len(s1)
         left = 0
-        window = deque(s2[:k])
+        check = defaultdict(int)
 
         count = Counter(s1)
 
-        for i in range(k):
-            if Counter(window) == count:
+        for right in range(len(s2)):
+            check[s2[right]] += 1
+
+            while right - left + 1 > k:
+                check[s2[left]] -= 1
+               
+                if check[s2[left]] == 0:
+                    del check[s2[left]]
+                left += 1
+         
+            if check == count:
                 return True
-        
-        for right in range(k , len(s2)):
-            window.popleft()
-            left += 1
-            window.append(s2[right])
-            if Counter(window) == count:
-                return True
-        
+
         return False
+
 
 
 
