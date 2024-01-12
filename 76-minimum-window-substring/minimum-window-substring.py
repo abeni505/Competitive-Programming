f96c32200a29431cce4d931846c571b003381fd2
Abenezer Mulugeta <70083175+abeni505@users.noreply.github.com>
2024-01-12 22:58:33 +0300

    Time: 6936 ms (5.01%) | Memory: 18.3 MB (7.70%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.py
+++ 76-minimum-window-substring/minimum-window-substring.py
@@ -3,8 +3,7 @@ class Solution:
 
         check = Counter(t)
         window = defaultdict(int)
-
-        output = []
+        window_d = deque()
 
         left = 0
         min_len = float('inf') 
@@ -12,15 +11,20 @@ class Solution:
         for right in range(len(s)):
 
             window[s[right]] += 1
+            window_d.append(s[right])
             while all(window[key] >= value for key, value in check.items()):
                 if right - left + 1 < min_len:
                     min_len = right - left + 1
-                    output = [s[left:right + 1]]
+                    window_min = deque(window_d)
             
                 window[s[left]] -= 1
+                window_d.popleft()
                 if window[s[left]] == 0:
                     del window[s[left]]
-                    
+
                 left += 1
 
-        return output[0] if output else ""
+        if min_len == float('inf'):
+            return ""
+
+        return "".join(window_min)
