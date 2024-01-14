e68f66cfabd63011caf237fe8a29525265dacc81
Abenezer Mulugeta <70083175+abeni505@users.noreply.github.com>
2024-01-14 14:58:17 +0300

    Time: 117 ms (93.05%) | Memory: 18.3 MB (38.39%) - LeetSync

--- 1777-determine-if-two-strings-are-close/determine-if-two-strings-are-close.py
+++ 1777-determine-if-two-strings-are-close/determine-if-two-strings-are-close.py
@@ -5,7 +5,11 @@ class Solution:
 
         val1 = Counter(count1.values())
         val2 = Counter(count2.values())
-        
 
+        # check set difference if they have equal distribution
+        # print(val1,val2)
+        # print(val1 - val2)
+
+        # print(count1.keys() , count2.keys())
         return len(val1 - val2) == 0 and count1.keys() == count2.keys()
      
