2bd96e2ae859f2351c185b7ea1907544a628da2f
Abenezer Mulugeta <70083175+abeni505@users.noreply.github.com>
2024-01-15 23:48:09 +0300

    Time: 422 ms (31.86%) | Memory: 17.6 MB (7.96%) - LeetSync

---
+++ 457-circular-array-loop/circular-array-loop.py
@@ -0,0 +1,21 @@
+class Solution:
+    def circularArrayLoop(self, nums: List[int]) -> bool:
+        def next_index(current):
+           return (current + nums[current]) % len(nums)
+
+
+        for root_index in range(len(nums)):
+            current_index = root_index
+            next_ = next_index(current_index)
+            # print(current_index ,next_)
+            visited = set()
+
+            while current_index not in visited and nums[next_index(current_index)] * nums[current_index] > 0:
+                visited.add(current_index)
+                current_index = next_index(current_index)
+            # print(visited)
+   
+            if current_index == root_index and len(visited) > 1:
+                return True
+        
+        return False
