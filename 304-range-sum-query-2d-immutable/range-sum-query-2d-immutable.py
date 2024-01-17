a0a6c968aabcf78b9289a60f772cdd29d7a94f02
Abenezer Mulugeta <70083175+abeni505@users.noreply.github.com>
2024-01-17 20:08:29 +0300

    Time: 1024 ms (78.33%) | Memory: 28.3 MB (25.51%) - LeetSync

--- 304-range-sum-query-2d-immutable/range-sum-query-2d-immutable.py
+++ 304-range-sum-query-2d-immutable/range-sum-query-2d-immutable.py
@@ -7,16 +7,27 @@ class NumMatrix:
 
         for r in range(row):
             for c in range(col):
-                self.prefix[r + 1][c + 1] = self.prefix[r + 1][c] + self.prefix[r][c + 1] - self.prefix[r][c] + matrix[r][c]
+                left_area = self.prefix[r + 1][c]
+                right_area = self.prefix[r][c + 1]
+                duplicated_area = self.prefix[r][c]
+                curr_element = matrix[r][c]
+
+                self.prefix[r + 1][c + 1] =  left_area + right_area - duplicated_area + curr_element
         
-        print(self.prefix)
+       
 
     def sumRegion(self, row1: int, col1: int, row2: int, col2: int) -> int:
         row2 = row2 + 1
         row1 = row1 + 1
         col1 = col1 + 1
         col2 = col2 + 1
-        return self.prefix[row2][col2] - self.prefix[row2][col1 - 1] - self.prefix[row1 - 1][col2] + self.prefix[row1 - 1][col1 - 1]
+        
+        Total_area = self.prefix[row2][col2]
+        l_area = self.prefix[row2][col1 - 1]
+        r_area = self.prefix[row1 - 1][col2]
+        dup_area = self.prefix[row1 - 1][col1 - 1]
+
+        return  Total_area - l_area - r_area + dup_area
 
 
         
