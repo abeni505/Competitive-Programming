1ed056e438d65632e7bf6cabfa32289276c95a27
Abenezer Mulugeta <70083175+abeni505@users.noreply.github.com>
2024-01-21 01:22:56 +0300

    Time: 78 ms (94.16%) | Memory: 17.9 MB (56.40%) - LeetSync

--- 1242-matrix-block-sum/matrix-block-sum.py
+++ 1242-matrix-block-sum/matrix-block-sum.py
@@ -3,24 +3,26 @@ class Solution:
         row = len(mat)
         col = len(mat[0])
 
-        prefix = mat[:][:]
+
 
         for r in range(row):
             for c in range(col):
-                left = prefix[r][c - 1] if c > 0 else 0
-                up = prefix[r - 1][c] if r > 0 else 0
-                diagonal = prefix[r - 1][c - 1] if r > 0 and c > 0 else 0
+                left = mat[r][c - 1] if c > 0 else 0
+                up = mat[r - 1][c] if r > 0 else 0
+                diagonal = mat[r - 1][c - 1] if r > 0 and c > 0 else 0
 
-                prefix[r][c] = mat[r][c] + left + up - diagonal
-        # print(prefix)
+                mat[r][c] = mat[r][c] + left + up - diagonal
+        # print(mat)
+
+
         prefix_new = [[0 for c in range(col)] for r in range(row)]
 
         for i in range(row):
             for j in range(col):
-                diagonal_bottom = prefix[min(i+k, row-1)][min(j+k, col-1)]
-                diagonal_top = prefix[i-k-1][j-k-1] if i-k > 0 and j-k > 0 else 0
-                top_right = prefix[i-k-1][min(j+k, col-1)] if i-k > 0 else 0
-                bottom_left = prefix[min(i+k, row-1)][j-k-1] if j-k > 0 else 0
+                diagonal_bottom = mat[min(i+k, row-1)][min(j+k, col-1)]
+                diagonal_top = mat[i-k-1][j-k-1] if i-k > 0 and j-k > 0 else 0
+                top_right = mat[i-k-1][min(j+k, col-1)] if i-k > 0 else 0
+                bottom_left = mat[min(i+k, row-1)][j-k-1] if j-k > 0 else 0
 
                 prefix_new[i][j] = diagonal_bottom - top_right - bottom_left + diagonal_top
 
