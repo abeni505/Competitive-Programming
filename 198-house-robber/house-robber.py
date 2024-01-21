34fbf86cf8658f900253a9991c65e8d801f6a998
Abenezer Mulugeta <70083175+abeni505@users.noreply.github.com>
2024-01-21 12:13:37 +0300

    Time: 40 ms (46.58%) | Memory: 16.5 MB (60.22%) - LeetSync

--- 198-house-robber/house-robber.py
+++ 198-house-robber/house-robber.py
@@ -1,5 +1,6 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
+        
         if len(nums) == 1:
             return nums[0]
 
@@ -7,7 +8,7 @@ class Solution:
         dp[0] = nums[0]
         dp[1] = nums[1]
 
-        for left in range(2, len(nums)):
-            dp[left] = max(nums[left], nums[left] + max(dp[:left - 1]))
+        for i in range(2,len(nums)):
+            dp[i] =  max(dp[i] ,(nums[i] + max(dp[:i - 1])))
 
         return max(dp)
