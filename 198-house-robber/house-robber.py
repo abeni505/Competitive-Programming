33b0c0d491dead1f8bab69a3cdedacc4d6d8b04b
Abenezer Mulugeta <70083175+abeni505@users.noreply.github.com>
2024-01-21 12:17:40 +0300

    Time: 38 ms (62.70%) | Memory: 16.6 MB (56.96%) - LeetSync

--- 198-house-robber/house-robber.py
+++ 198-house-robber/house-robber.py
@@ -6,9 +6,9 @@ class Solution:
 
         dp = [0] * len(nums)
         dp[0] = nums[0]
-        dp[1] = nums[1]
+        dp[1] = max(nums[0], nums[1])
 
         for i in range(2,len(nums)):
-            dp[i] =  max(dp[i] ,(nums[i] + max(dp[:i - 1])))
+            dp[i] = max(dp[i - 1], dp[i - 2] + nums[i])
 
-        return max(dp)
+        return dp[-1]
