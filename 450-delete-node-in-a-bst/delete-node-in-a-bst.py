1709c0a18d629c9b63774578933a3e92603f412d
Abenezer Mulugeta <70083175+abeni505@users.noreply.github.com>
2024-02-28 16:54:10 +0300

    Time: 51 ms (87.06%) | Memory: 19.9 MB (78.13%) - LeetSync

---
+++ 450-delete-node-in-a-bst/delete-node-in-a-bst.py
@@ -0,0 +1,53 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def deleteNode(self, root: Optional[TreeNode], key: int) -> Optional[TreeNode]:
+        
+        # inorder = [] 
+        # # hashmap = defaultdict(int)
+        # def inorder_t(root):
+        #     if not root:
+        #         return
+        #     inorder_t(root.left)
+        #     inorder.append(root.val)
+        #     # hashmap[root.val] = root
+        #     inorder_t(root.right)
+        # inorder_t(root)
+        def helper(root):
+            curr = root
+            while curr.left:
+                curr = curr.left
+            
+            return curr
+
+        def delete(root , key):
+            if not root:
+                return 
+            if root.val == key:
+                if root.left and root.right:
+                    # key = inorder[inorder.index(key) + 1]
+                    temp = helper(root.right)
+                    root.val = temp.val
+
+                    root.right = delete(root.right , temp.val)
+                
+                elif (not root.left and root.right):
+                    return root.right
+                elif not root.right and root.left:
+                    return root.left
+                else:
+                    return None
+
+            
+            elif root.val < key:
+                root.right = delete(root.right , key)
+            else:
+                root.left = delete(root.left , key)
+
+            return root
+
+        return delete(root , key)
