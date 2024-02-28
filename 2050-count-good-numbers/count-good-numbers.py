cf2202534d0c0bf27978f4130475310af4293b79
Abenezer Mulugeta <70083175+abeni505@users.noreply.github.com>
2024-02-28 18:44:29 +0300

    Time: 31 ms (85.30%) | Memory: 16.5 MB (74.86%) - LeetSync

--- 2050-count-good-numbers/count-good-numbers.py
+++ 2050-count-good-numbers/count-good-numbers.py
@@ -1,6 +1,30 @@
 class Solution:
     def countGoodNumbers(self, n: int) -> int:
         
-        mod = 10**9 + 7
+        mod =  (10**9 + 7)
+        modinverse = 400000003
+        def modDivide(a,b,m):
+            a = a % m
+            inv = modinverse
+            return (inv*a) % m
 
-        return (pow(5 ,(n + 1)//2 , mod) * pow(4 ,(n//2) , mod)) % mod
+        if n == 1:
+            return 5
+        if n == 2:
+            return 20
+
+
+        res = self.countGoodNumbers(n//2)
+        if (n//2) % 2:
+            a = (res*res * 4)
+            b = 5
+            final = modDivide(a,b,mod)
+        else:
+            final = (res * res ) % mod
+
+        if n % 2:
+            final = (final * 5) % mod
+        
+        
+        
+        return final
