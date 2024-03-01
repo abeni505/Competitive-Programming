f15426d2fd08e4ac2e4daeb7d8d7fbd2b4d93888
Abenezer Mulugeta <70083175+abeni505@users.noreply.github.com>
2024-03-01 14:57:26 +0300

    Add solution for Put Marbles in Bags

--- leetcode/put-marbles-in-bags.py
+++ leetcode/put-marbles-in-bags.py
@@ -1,22 +1,6 @@
 class Solution:
     def putMarbles(self, weights: List[int], k: int) -> int:
-
-        new = []
-        for i in range(1,len(weights)):
-            new.append(weights[i] + weights[i - 1])
         
-        new = sorted(new , reverse = True)
-        max_diff = 0
-        print(new)
-
-        left = 0
-        right = len(new) - 1
-        while (k - 1):
-            max_diff += new[left] - new[right]
-            left += 1
-            right -= 1
-            
-            k-=1
-        
-        return max_diff
-        
+        new = sorted(weights[i] + weights[i - 1] for i in range(1,len(weights)))
+ 
+        return sum(new[len(new) - k + 1: ]) - sum(new[:k - 1])
