f444005c6232a26f6e91fb4bd4f946927bc13c4d
Abenezer Mulugeta <70083175+abeni505@users.noreply.github.com>
2024-03-05 01:54:10 +0300

    Time: 30 ms (98.02%) | Memory: 16.5 MB (97.33%) - LeetSync

--- 46-permutations/permutations.py
+++ 46-permutations/permutations.py
@@ -1,19 +1,5 @@
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
         
-        output = []
-        if len(nums) == 1:
-            return [nums.copy()]
-        
-        for i in range(len(nums)):
-            poped = nums.pop(0)
-
-            perms = self.permute(nums)
-
-            for perm in perms:
-                perm.append(poped)
-            
-            output.extend(perms)
-            nums.append(poped)
-
-        return output
+       
+        return list(permutations(nums))
