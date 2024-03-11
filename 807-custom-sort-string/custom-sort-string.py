bd3ada90e72d605ee225e291a76d0529ef6ba9b6
Abenezer Mulugeta <70083175+abeni505@users.noreply.github.com>
2024-03-11 22:02:05 +0300

    Time: 34 ms (68.03%) | Memory: 16.6 MB (61.09%) - LeetSync

--- 807-custom-sort-string/custom-sort-string.py
+++ 807-custom-sort-string/custom-sort-string.py
@@ -1,12 +1,13 @@
 class Solution:
     def customSortString(self, order: str, s: str) -> str:
         
+       
         count = Counter(s)
         output = []
         new = []
         
         for i in order:
-            if i in s:
+            if i in count:
                 for _ in range(count[i]):
                     output.append(i)
                 del count[i]
