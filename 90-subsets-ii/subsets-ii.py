73289f8a0ce5690f533c35deca90847683fc251b
Abenezer Mulugeta <70083175+abeni505@users.noreply.github.com>
2024-03-22 12:23:54 +0300

    Time: 31 ms (93.95%) | Memory: 16.9 MB (17.95%) - LeetSync

---
+++ 90-subsets-ii/subsets-ii.py
@@ -0,0 +1,21 @@
+class Solution:
+    def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
+        
+        ans = set()
+
+        def backtrack(index , currans):
+            if index == len(nums):
+
+                ans.add(tuple(sorted(currans)))
+                return
+
+            currans.append(nums[index])
+            backtrack(index + 1 , currans)
+
+            currans.pop()
+            backtrack(index + 1, currans)
+
+        backtrack(0 , [])
+
+        return ans
+        
