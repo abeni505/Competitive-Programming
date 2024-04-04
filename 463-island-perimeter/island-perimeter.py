de0d163657ac803703a8240764e652005d898d6a
Abenezer Mulugeta <70083175+abeni505@users.noreply.github.com>
2024-04-04 22:07:44 +0300

    Time: 533 ms (8.50%) | Memory: 26.1 MB (5.04%) - LeetSync

--- 463-island-perimeter/island-perimeter.py
+++ 463-island-perimeter/island-perimeter.py
@@ -1,27 +1,36 @@
 class Solution:
     def islandPerimeter(self, grid: List[List[int]]) -> int:
         
+        visited = set()
         drxn = [(0, 1),(0, -1),(1, 0),(-1, 0)]
         def inbound(row, col):
             return (0 <= row < len(grid) and 0 <= col < len(grid[0]))
 
+        
         count = 0
+        def dfs(row, col):
+            nonlocal count
+            for a, b in drxn:
+                newr = row + a
+                newc = col + b
+
+                if inbound(newr, newc) and  grid[newr][newc] == 1 :
+
+                    if (newr, newc) not in visited:
+                        visited.add((newr, newc))
+                        dfs(newr, newc)
+                
+                else:
+                    count += 1
+              
         for row in range(len(grid)):
             for col in range(len(grid[0])):
-                
-                if grid[row][col] == 1:
-                    stack = [(row,col)]
+                if grid[row][col] == 1 and (row, col) not in visited:
 
-                    while stack:
-                        r , c = stack.pop()
-                        for a , b in drxn:
-                            newr = r + a
-                            newc = c + b
+                    visited.add((row, col))
+                    dfs(row, col)
 
-                            if not inbound(newr,newc) or grid[newr][newc] == 0:
-                                count += 1
-        
         return count
-                            
+
 
                         
