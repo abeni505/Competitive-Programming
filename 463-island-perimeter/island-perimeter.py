ecee2535a8cf7eb08c5534eb2c06572ab0d91f27
Abenezer Mulugeta <70083175+abeni505@users.noreply.github.com>
2024-04-04 21:23:02 +0300

    Time: 461 ms (23.90%) | Memory: 16.9 MB (92.35%) - LeetSync

--- 463-island-perimeter/island-perimeter.py
+++ 463-island-perimeter/island-perimeter.py
@@ -1,23 +1,25 @@
 class Solution:
     def islandPerimeter(self, grid: List[List[int]]) -> int:
         
-        drxn = [(0 , 1),(0 , -1),(1 , 0),(-1 , 0)]
+        drxn = [(0, 1),(0, -1),(1, 0),(-1, 0)]
         def inbound(row, col):
             return (0 <= row < len(grid) and 0 <= col < len(grid[0]))
 
         count = 0
         for row in range(len(grid)):
             for col in range(len(grid[0])):
-                print(row,col)
+                
                 if grid[row][col] == 1:
-                    count += 4
+                    stack = [(row,col)]
 
-                    for a , b in drxn:
-                        newr = row + a
-                        newc = col + b
+                    while stack:
+                        r , c = stack.pop()
+                        for a , b in drxn:
+                            newr = r + a
+                            newc = c + b
 
-                        if inbound(newr,newc) and grid[newr][newc] == 1:
-                            count -= 1
+                            if not inbound(newr,newc) or grid[newr][newc] == 0:
+                                count += 1
         
         return count
                             
