005383c9007be00f833318b8a840ab4a2461aaaf
Abenezer Mulugeta <70083175+abeni505@users.noreply.github.com>
2024-04-05 16:33:32 +0300

    Time: 84 ms (85.85%) | Memory: 18.2 MB (80.87%) - LeetSync

--- 207-course-schedule/course-schedule.py
+++ 207-course-schedule/course-schedule.py
@@ -7,12 +7,22 @@ class Solution:
 
         state = [0] * numCourses
 
+    
+        # Each vertex can have 3 different states:
+        # state 0   : vertex is not visited. It's a default state.
+        # state -1  : vertex is being processed. Either all of its descendants
+        #             are not processed or it's still in the function call stack.
+        # state 1   : vertex and all its descendants are processed.
+    
         def hasCycle(v):
             if state[v] == 1:
+                # This vertex is processed so we pass.
                 return False
             if state[v] == -1:
+                # This vertex is being processed and it means we have a cycle.
                 return True
 
+            # Set state to -1
             state[v] = -1
 
             for i in adjlst[v]:
@@ -21,10 +31,10 @@ class Solution:
 
             state[v] = 1
             return False
-        
+
+        # we traverse each vertex using DFS, if we find a cycle, stop and return
         for v in range(numCourses):
             if hasCycle(v):
                 return False
 
         return True
-            
