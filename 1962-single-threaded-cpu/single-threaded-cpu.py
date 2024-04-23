7c2106c045992d4fa55753910e4c3ba25a31a513
Abenezer Mulugeta <70083175+abeni505@users.noreply.github.com>
2024-04-23 16:28:25 +0300

    Time: 1388 ms (89.64%) | Memory: 66.2 MB (9.59%) - LeetSync

--- 1962-single-threaded-cpu/single-threaded-cpu.py
+++ 1962-single-threaded-cpu/single-threaded-cpu.py
@@ -4,66 +4,25 @@ class Solution:
 
         minheap = []
         
-        for i in range(len(tasks)):
-            arrivaltime , processtime = tasks[i]
-            tasks[i] = (arrivaltime,processtime,i)
-        tasks.sort()
+        tasks = sorted((tasks[i][0] , tasks[i][1] , i) for i in range(len(tasks)))
 
         output = []
         time = 0
         for arrival_time, process_time, indx in tasks:
-    
+           
             while minheap and arrival_time > time:
-                next_process_time, next_indx, next_end_time = heappop(minheap)
-                output.append(next_indx)
-                
-                time += next_process_time
+                min_pt , min_indx = heappop(minheap)
+                output.append(min_indx)
+
+                time += min_pt
 
             time = max(time, arrival_time)
-            end_time = time + process_time
-            heappush(minheap, (process_time, indx, end_time))
+            heappush(minheap, (process_time, indx))
 
         for i in range(len(minheap)):
-            output.append(heappop(minheap)[1])
-       
-        return output 
+            output.append(heappop(minheap)[-1])
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-            # if not minheap:
-            #     output.append(i)
-            #     heappush(minheap, (processtime , arrivaltime ,endtime, i)
-            # else:
-                
-            #     while minheap:
-            #         pt , at , indx, endt = heapopp(minheap)
-
-            #         heappush(minheap, (processtime , arrivaltime , i)
-
-            #         if arrivaltime < endt:
-            #             heappush(minheap,pt, at , indx ,endt)
-
-            #         elif arrivaltime >= endtime:
-            #             if arrivaltime == endtime:
-            #                 a, b , c, d = heappop(minheap)
+        return output
 
 
 
