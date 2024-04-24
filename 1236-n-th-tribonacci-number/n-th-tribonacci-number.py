0f79963f972a9ae0317ccaf820a1e02e996588df
Abenezer Mulugeta <70083175+abeni505@users.noreply.github.com>
2024-04-24 22:20:21 +0300

    Time: 24 ms (97.52%) | Memory: 16.5 MB (31.32%) - LeetSync

--- 1236-n-th-tribonacci-number/n-th-tribonacci-number.py
+++ 1236-n-th-tribonacci-number/n-th-tribonacci-number.py
@@ -1,11 +1,11 @@
 class Solution:
     def tribonacci(self, n: int) -> int:
         
-        memo = defaultdict(int)
+        memo = [0 , 1 , 1]
 
-        memo[0] , memo[1] , memo[2] = 0 , 1 , 1
+        if n < 3:
+            return memo[n]
+        for i in range(3 , n + 1):
+            memo[0] , memo[1] , memo[2] = memo[1] , memo[2] , sum(memo)
 
-        for i in range(3, n + 1):
-            memo[i] = memo[i - 1] + memo[i - 2] + memo[i - 3]
-      
-        return memo[n]
+        return memo[2]
