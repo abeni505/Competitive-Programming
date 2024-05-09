940abc3cddab720e3f06310dfec788714bf83162
Abenezer Mulugeta <70083175+abeni505@users.noreply.github.com>
2024-05-09 21:55:30 +0300

    Time: 38 ms (43.94%) | Memory: 16.7 MB (12.20%) - LeetSync

--- 198-house-robber/house-robber.py
+++ 198-house-robber/house-robber.py
@@ -1,15 +1,12 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        memo = {}
+        @cache
         def dp(i):
             if i == 0:
                 return nums[0]
             if i == 1:
-                return max(nums[0], nums[1])
+                return max(nums[0], nums[1])     
             
-            if i not in memo:
-                memo[i] = max(dp(i - 1) , nums[i] + dp(i - 2))
-            
-            return memo[i]
+            return max(dp(i - 1) , nums[i] + dp(i - 2))
 
         return dp(len(nums) - 1)
