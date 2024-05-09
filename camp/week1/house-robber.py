49d2c40dc382c5b3686de6ed0642d008922e70f9
Abenezer Mulugeta <70083175+abeni505@users.noreply.github.com>
2024-05-09 22:19:49 +0300

    Added solution for House Robber

---
+++ camp/week1/house-robber.py
@@ -0,0 +1,12 @@
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        @cache
+        def dp(i):
+            if i == 0:
+                return nums[0]
+            if i == 1:
+                return max(nums[0], nums[1])     
+            
+            return max(dp(i - 1) , nums[i] + dp(i - 2))
+
+        return dp(len(nums) - 1)
