cd5ceefba7d867bd80ca2415cdaf4e08eaa639ba
Abenezer Mulugeta <70083175+abeni505@users.noreply.github.com>
2024-05-15 18:35:08 +0300

    Time: 43 ms (10.17%) | Memory: 16.8 MB (11.09%) - LeetSync

---
+++ 62-unique-paths/unique-paths.py
@@ -0,0 +1,25 @@
+class Solution:
+    def uniquePaths(self, m: int, n: int) -> int:
+
+        def inbound(row, col):
+            return 0 <= row < m and 0 <= col < n
+        
+        memo = {}
+        def dp(row, col):
+
+            if (row , col) in memo:
+                return memo[(row,col)]
+
+            if row == m-1 and col == n-1:
+                return 1
+            
+            if not inbound(row , col):
+                return 0
+
+            memo[(row,col)] = dp(row + 1, col) + dp(row , col + 1)
+            return memo[(row,col)]
+
+        return dp(0,0)
+
+
+        
