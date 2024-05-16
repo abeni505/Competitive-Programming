8e7293e8702739883739e4b7f1cb14a3a7e51890
Abenezer Mulugeta <70083175+abeni505@users.noreply.github.com>
2024-05-16 13:19:18 +0300

    Time: 39 ms (85.65%) | Memory: 17.5 MB (9.99%) - LeetSync

--- 1025-minimum-cost-for-tickets/minimum-cost-for-tickets.py
+++ 1025-minimum-cost-for-tickets/minimum-cost-for-tickets.py
@@ -3,7 +3,7 @@ class Solution:
         
         days = set(days)
         last_day = max(days)
-        
+
         memo = {}
         def dp(day):
             if day in memo:
@@ -11,22 +11,12 @@ class Solution:
 
             if day > last_day:
                 return 0
-    
-            minn = float('inf')
 
             if day in days:
-                for i in range(3):
-                    if i == 0:
-                        minn = min(minn, costs[i] + dp(day + 1))
-                    elif i == 1:
-                        minn = min(minn , costs[i] + dp(day + 7))
-                    else:
-                        minn = min(minn, costs[i] + dp(day + 30))
-                    
-                    memo[day] = minn
+                memo[day] = min(costs[0] + dp(day + 1), costs[1] + dp(day + 7), costs[2] + dp(day + 30))
+     
             else:
-                memo[day] = dp(day + 1)
-                
+                memo[day] = dp(day + 1)      
            
             return memo[day]
         
