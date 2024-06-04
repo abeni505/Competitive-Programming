b3e6acb4e0cbc5dfef543fce3fcd8f05657312ee
Abenezer Mulugeta <70083175+abeni505@users.noreply.github.com>
2024-06-04 22:14:26 +0300

    Time: 44 ms (11.03%) | Memory: 16.6 MB (80.97%) - LeetSync

--- 409-longest-palindrome/longest-palindrome.py
+++ 409-longest-palindrome/longest-palindrome.py
@@ -1,18 +1,18 @@
 class Solution:
     def longestPalindrome(self, s: str) -> int:
         
-        max_len = 0
         count = Counter(s)
-
-        odd = False
-
-        for val in count.values():
-            if val & 1:
-                odd = True
-                max_len += val -1
+        
+        ans = max_odd = 0
+        for key , val in count.items():
+            if val % 2:
+                max_odd = max(max_odd , val)
+                ans += val - 1
             else:
-                max_len += val
-        if odd:
-            max_len += 1
-            
-        return max_len
+                ans += val
+                
+        if max_odd:
+            ans += 1
+
+        return ans
+
