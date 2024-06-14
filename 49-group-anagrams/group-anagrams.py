b93319179b9f932813911eaf7ea957075fd06864
Abenezer Mulugeta <70083175+abeni505@users.noreply.github.com>
2024-06-14 19:57:30 +0300

    Time: 90 ms (55.21%) | Memory: 21.4 MB (30.59%) - LeetSync

--- 49-group-anagrams/group-anagrams.py
+++ 49-group-anagrams/group-anagrams.py
@@ -1,12 +1,19 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        hashmap=defaultdict(list)
-        for s in strs:
-            count =[0]*26
+        
+        hashmap = defaultdict(list)
 
-            for char in s:
-                count[ord(char)-ord("a")]+=1
-
-            hashmap[tuple(count)].append(s)
- 
-        return list(hashmap.values())
+        for i in range(len(strs)):
+            count = sorted(strs[i])
+            hashmap[tuple(count)].append(i)
+            
+        
+        output = []
+        for key , val in hashmap.items():
+            curr  = []
+            for j in val:
+                curr.append(strs[j])
+            
+            output.append(curr)
+            
+        return output
