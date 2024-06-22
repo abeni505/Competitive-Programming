a57b24150a155afbff2047ad0b0c4c097482f29c
Abenezer Mulugeta <70083175+abeni505@users.noreply.github.com>
2024-06-22 13:55:32 +0300

    Time: 736 ms (6.64%) | Memory: 25.2 MB (18.00%) - LeetSync

--- 1370-count-number-of-nice-subarrays/count-number-of-nice-subarrays.py
+++ 1370-count-number-of-nice-subarrays/count-number-of-nice-subarrays.py
@@ -1,23 +1,23 @@
 class Solution:
     def numberOfSubarrays(self, nums: List[int], k: int) -> int:
-        def atmost(k):
-            left = count_odd = 0
-            n = len(nums)
-            total = 0
-
-            for right in range(n):
+        
+        def atmostK(k):
+            left = 0
+            window = set()
+            odd_count = total_count = 0
+            for right in range(len(nums)):
+                window.add(nums[right])
+                if nums[right] % 2:
+                    odd_count += 1
                 
-
-                if nums[right] & 1:
-                    count_odd += 1
-            
-                while count_odd > k:
-                    if nums[left] & 1:
-                       count_odd -= 1
-
+                while odd_count > k:
+                    if nums[left] % 2:
+                        odd_count -= 1
                     left += 1
-                total += right - left + 1
-                
-            return total
-                
-        return atmost(k) - atmost(k - 1)
+
+                total_count += right - left + 1
+
+            return total_count
+        
+        return atmostK(k) - atmostK(k - 1)
+
