f1b7d1ad5cbaf8ccebf5182fbe629c10f0dd32f6
Abenezer Mulugeta <70083175+abeni505@users.noreply.github.com>
2024-07-24 22:12:53 +0300

    Time: 1216 ms (21.74%) | Memory: 24.7 MB (60.87%) - LeetSync

---
+++ 1333-sort-the-jumbled-numbers/sort-the-jumbled-numbers.py
@@ -0,0 +1,19 @@
+class Solution:
+    def sortJumbled(self, mapping, nums):
+        store_pairs = []
+
+        for i in range(len(nums)):
+            number = str(nums[i])
+            formed = ""
+            for j in range(len(number)):
+                formed = formed + str(mapping[int(number[j])])
+            
+            mapped_value = int(formed)
+            store_pairs.append((mapped_value, i))
+
+        store_pairs.sort()
+        answer = []
+        for pair in store_pairs:
+            answer.append(nums[pair[1]])
+            
+        return answer
